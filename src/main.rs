66faa5f8ea7b45f3fdb2042130fdaf54af52a4bc
Wojciech Bartnik <yisonPylkita@gmail.com>
2021-04-23 20:15:18 +0200

    Utc time is not mocked anymore

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
+use chrono::prelude::*;
 use std::ffi::OsString;
 
-use anyhow::Result;
-
-const PROMPT_CAPACITY: usize = 1024;
+const PROMPT_CAPACITY: usize = 256;
 
 type ShellColor = str;
 const SHELL_COLOR_RED: &ShellColor = "\\033[0;31m";
@@ -16,18 +15,33 @@ fn with_color(input: String, color: &ShellColor) -> String {
     result
 }
 
+fn get_utc_time() -> String {
+    Local::now().format("%H:%M:%S").to_string()
+}
+
 fn get_hostname() -> OsString {
     hostname::get().unwrap()
 }
 
+fn get_current_working_directory_path() -> String {
+    std::env::current_dir()
+        .unwrap()
+        .to_str()
+        .unwrap()
+        .to_string()
+}
+
 fn main() {
     let mut prompt = String::with_capacity(PROMPT_CAPACITY);
-    prompt.push_str("18:55:37 ");
+    prompt.push_str(&get_utc_time());
+    prompt.push_str(" ");
     prompt.push_str(&with_color(
         get_hostname().to_str().unwrap().to_string(), // TODO: Refactor this
         SHELL_COLOR_GREEN,
     ));
     prompt.push_str(" ");
+    prompt.push_str(&get_current_working_directory_path());
+    prompt.push_str(" >");
 
     print!("{}", prompt);
 }
